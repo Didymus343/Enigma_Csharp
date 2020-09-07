578fbd92406bbb39ae49a39b8811ff2f61c903fb
Didymus343 <th-jansen@hotmail.com>
2020-09-07 07:57:53 +0200

    Dialog changes

--- Enigma/View/enigma.xaml.cs
+++ Enigma/View/enigma.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Enigma
+namespace Enigma.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for enigma.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class enigma : Window
     {
-        public MainWindow()
+        public enigma()
         {
             InitializeComponent();
         }
---
+++ Enigma/ViewModel/UserViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Enigma.Model;
+using System.ComponentModel;
+
+namespace Enigma.ViewModel
+{
+    public class UserViewModel: INotifyPropertyChanged
+    {
+        private User user;
+
+        public UserViewModel()
+        {
+            user = new User
+            {
+                FirstName = "Thomas",
+                LastName = "Jansen"
+            };
+
+        }
+
+
+        public string FirstName
+        {
+            get { return user.FirstName; }
+            set
+            {
+                if (user.FirstName.Equals(value)) return;
+
+                user.FirstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return user.LastName; }
+            set
+            {
+                if (user.LastName.Equals(value)) return;
+
+                user.LastName = value;
+            }
+        }
+
+        public string FullName
+        {
+        get { return FirstName + " " + LastName; }
+                
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
